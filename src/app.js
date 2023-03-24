4c37d1fa0a9fd74ca3207371fcda27fe9a61e2fd
Gabriel Sinapsis <gabyreload@gmail.com>
2023-03-24 16:15:05 -0300

    dinamic port

--- src/app.js
+++ src/app.js
@@ -28,4 +28,5 @@ try {
   console.log(error);
 }
 
-app.listen(8080, () => console.log("Listening on port 8080"));
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log("Listening on port 8080"));
